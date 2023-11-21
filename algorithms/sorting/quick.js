dc3acc1063a23eca8a7a8a50973395444134438d
Karol Brzegowy <karol.brzegowy@arteia.com>
2023-11-21 07:22:23 +0100

    Fix quicksort

--- algorithms/sorting/quick.js
+++ algorithms/sorting/quick.js
@@ -5,7 +5,7 @@ function quicksort(arr, left, right) {
     if (left < right) {
         const pivot = partition(arr, left, right);
         console.log(`1) quicksort(${left}, ${pivot})`);
-        quicksort(arr, left, pivot);
+        quicksort(arr, left, pivot - 1);
         console.log(`2) quicksort(${pivot + 1}, ${right})`);
         quicksort(arr, pivot + 1, right);
     }
@@ -15,7 +15,7 @@ function partition(arr, left, right) {
     const pivot = right;
     let i = left - 1;
 
-    for (let j = left; j < right - 1; j++) {
+    for (let j = left; j <= right - 1; j++) {
         if (arr[j] < arr[pivot]) {
             i++;
             swap(arr, i, j);
